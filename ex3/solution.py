5ee2bc6c328e51049863050c3f075b864d089c9f
Noam Ben Shimon <Noamben@campus.technion.ac.il>
2025-06-23 20:09:44 +0300

    I think I am through the worst of it?

--- ex3/solution.py
+++ ex3/solution.py
@@ -58,7 +58,59 @@ def find_solution(
     Where P, Q are assertions, and stmt is the modern AST.
     Returns a z3.Solver object, ready to be checked.
     """
-    raise NotImplementedError
+
+    def eval_expr(expr: Expr, env: Env) -> Formula:
+        match expr:
+            case Id(name):
+                return env[name]
+            case Int(value):
+                return z3.IntVal(value)
+            case BinOp(left, op, right):
+                evall = eval_expr(left, env)
+                evalr = eval_expr(right, env)
+                return OP[op](evall, evalr)
+        raise Exception(f"Unexpected expression: {expr}")
+
+    def weakest_precondition(stmt: Stmt, Q: Invariant) -> Invariant:
+        match stmt:
+            case Skip():
+                return Q
+            case Assign(var, expr):
+
+                def reQ(env: Env) -> Formula:
+                    evale = eval_expr(expr, env)
+                    new_env = upd(env, var, evale)
+                    return Q(new_env)
+
+                return reQ
+
+            case Seq(first, second):
+                # return weakest_precondition(first, weakest_precondition(second, Q))
+                precondition_from_second = weakest_precondition(second, Q)
+                precondition_from_first = weakest_precondition(first, precondition_from_second)
+                return precondition_from_first
+
+            case If(cond, then_branch, else_branch):
+
+                wp_then = weakest_precondition(then_branch, Q)
+                wp_else = weakest_precondition(else_branch, Q)
+
+                def reQ(env: Env) -> Formula:
+                    cond_formula = eval_expr(cond, env)
+
+                    precondition_then = z3.Implies(cond_formula, wp_then(env))
+                    precondition_else = z3.Implies(z3.Not(cond_formula), wp_else(env))
+
+                    return z3.And(precondition_then, precondition_else)
+
+                return reQ
+
+            case While(cond, body):
+                return linv
+
+        return
+
+    return z3.Solver()
 
 
 def verify(P: Invariant, stmt: Stmt, Q: Invariant, linv: Invariant = TRIVIAL) -> bool:
