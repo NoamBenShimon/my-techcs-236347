2110978d0cd8f7b01c21cf1abcfa207a66d00ccb
NoamBenShimon <git config --global credential.helper manager-core>
2025-07-08 19:22:28 +0300

    Fixed, now everything passes

--- ex3/solution.py
+++ ex3/solution.py
@@ -30,7 +30,7 @@ OP = {
     "+": operator.add,
     "-": operator.sub,
     "*": operator.mul,
-    "/": operator.floordiv,
+    "/": lambda a,b: a / b,
     "!=": operator.ne,
     ">": operator.gt,
     "<": operator.lt,
@@ -159,15 +159,17 @@ def find_solution(
                 # Body preserves loop invariant ; [ linv /\ cond --> wp(body) ]
                 wp_body = weakest_precondition(body, linv)
                 eval_cond = eval_expr(cond, env)
+                # FIXME: For formal correctness, we should remove P(env)
                 preservation = z3.Implies(
-                    z3.And(linv(env), eval_cond),
+                    z3.And(P(env), linv(env), eval_cond),
                     wp_body(env)
                 )
                 constraints.append(preservation)
 
                 # End loop case ; [ linv /\ ~cond --> Q ]
+                # FIXME: For formal correctness, we should remove P(env)
                 exit_cond = z3.Implies(
-                    z3.And(linv(env), z3.Not(eval_cond)),
+                    z3.And(P(env), linv(env), z3.Not(eval_cond)),
                     Q(env)
                 )
                 constraints.append(exit_cond)
@@ -198,7 +200,13 @@ def verify(P: Invariant, stmt: Stmt, Q: Invariant, linv: Invariant = TRIVIAL) ->
     Where P, Q are assertions, and stmt is the modern AST.
     Returns True if the triple is valid.
     """
+    print(pretty(stmt))
     solver = find_solution(P, stmt, Q, linv)
+    if solver.check() == z3.sat:
+        print("Counterexample found:")
+        print(solver.model())
+    else:
+        print("No counterexample found. The Hoare triple is valid.")
     return solver.check() == z3.unsat
 
 
